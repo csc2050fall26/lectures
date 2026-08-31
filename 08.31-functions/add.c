4e10e8a324cc21ca225be707acf74306e0dbf87a
Christopher E. Siu <cesiu@calpoly.edu>
2026-08-31 14:01:09 -0700

    Added example of functions and headers.

---
+++ 08.31-functions/add.c
@@ -0,0 +1,8 @@
+/* NOTE: This defines a function that takes two integers and returns an integer.
+ *       Note that the beginning of a function definition creates a new local
+ *       scope, and is thus a valid place to declare local variables. */
+int add(int x, int y) {
+    int z = x + y;
+
+    return z;
+}
--- 08.31-functions/test.c
+++ 08.31-functions/test.c
@@ -1,7 +1,14 @@
+/* NOTE: This instructs the compiler to textually copy-paste the contents of
+ *       "stdio.h" and "add.h" into to this file. Angle brackets indicate a
+ *       header in the standard library, whereas quotes indicate a header that
+ *       we ourselves created. */
 #include <stdio.h>
+#include "add.h"
 
 int main(void) {
-    printf("1 + 2: %d\n", 1 + 2);
+    printf("add(1, 2): %d\n", add(1, 2));
+    printf("add(-3, 4): %d\n", add(-3, 4));
+    printf("add(0, -5): %d\n", add(0, -5));
 
     return 0;
 }
