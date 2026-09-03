ef017e14538c98ec35768938f1b85957459831d0
Christopher E. Siu <cesiu@calpoly.edu>
2026-09-03 08:49:25 -0700

    Added examples of macros.

--- 09.02-macros/test.c
+++ 09.02-macros/test.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include "add.h"
 
+/* NOTE: This replaces just about any instance of the text "PI" with the text
+ *       "3.14"; note the absence of a trailing semicolon. */
+#define PI 3.14
+
+/* NOTE: This replaces just about any instance of the text of form "ADD(X, Y)"
+ *       with corresponding text of the form "X + Y". */
+#define ADD(X, Y) ((X) + (Y))
+
 int main(void) {
+    /* NOTE: Macros have the potential to be a lot more efficient than true
+     *       functions; it takes time to call a function. */
     printf("add(1, 2): %d\n", add(1, 2));
+    printf("ADD(1, 2): %d\n", ADD(1, 2));
+
+    /* NOTE: Macros can also be more flexible than functions, since their
+     *       arguments do not have to be declared with types. */
+    printf("add(PI, PI): %f\n", (double)add(PI, PI));
+    printf("ADD(PI, PI): %f\n", ADD(PI, PI));
+
+    /* NOTE: Since a macro is a naive search-and-replace, we should always add
+     *       parens to avoid inadvertent interactions with surrounding code.
+     * printf("ADD(PI-, PI): %f\n", ADD(PI-, PI)); */
+    printf("ADD(PI, PI) * 2: %f\n", ADD(PI, PI) * 2);
 
     return 0;
 }
