1f96544fbc0b3aedfbd3778639f64df30200094f
Christopher E. Siu <cesiu@calpoly.edu>
2026-09-08 08:48:27 -0700

    Added examples of pointers.

--- 09.08-pointers/test.c
+++ 09.08-pointers/test.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
-void swap(int, int);
+void swap(int *, int *);
 
 int main(void) {
     int z = 1, w = 2;
 
-    swap(z, w);
+    /* NOTE: Rather than passing copies of "z" and "w", this passes pointers to
+     *       them; rather than telling "swap" *what* the values of "z" and "w"
+     *       are, this tells it *where* in memory they can be found... */
+    swap(&z, &w);
 
-    printf("z: %d\n", z);
-    printf("w: %d\n", w);
+    /* NOTE: NULL is equivalent to 0 and guaranteed to be an invalid address
+     *       for data; the following is undefined and will likely cause a
+     *       segmentation fault attempting to dereference null pointers.
+     * swap(NULL, NULL); */
+
+    printf("z (%p): %d\n", (void *)&z, z);
+    printf("w (%p): %d\n", (void *)&w, w);
 
     return 0;
 }
 
-void swap(int x, int y) {
-    int temp = x;
-    x = y;
-    y = temp;
+void swap(int *x, int *y) {
+    /* NOTE: ...in C, arguments are always pass-by-value, but the value of a
+     *       pointer is a reference. Using pointers, we can effectively opt-in
+     *       to pass-by-reference behavior whenever we want. */
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    printf("x (%p): %p\n", (void *)&x, (void *)x);
+    printf("y (%p): %p\n", (void *)&y, (void *)y);
 }
