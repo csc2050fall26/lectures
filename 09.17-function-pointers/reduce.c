1da84c1e91b9c130d0e76a011fbbf10c0b0d8e88
Christopher E. Siu <cesiu@calpoly.edu>
2026-09-17 08:46:20 -0700

    Added example of function pointers.

--- 09.17-function-pointers/reduce.c
+++ 09.17-function-pointers/reduce.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
+#include "add.h"
+#include "max.h"
 
-int reduce(int[], int);
+int reduce(int[], int, int (*)(int, int));
 
 int main(void) {
     int arr[] = {2, -1, 9, 8, 5, -3, 0, 8};
 
-    printf("reduce(%p, 8): %d\n", (void *)arr, reduce(arr, 8));
+    /* NOTE: By passing a function pointer into "reduce" rather than having it
+     *       hard-code the reducing operation, we make "reduce" slower (since
+     *       it has to call the function) but more flexible (since we can pass
+     *       different pointers to different functions. */
+    printf("reduce(%p, 8, %p): %d\n",
+     (void *)arr, (void *)add, reduce(arr, 8, add));
+
+    printf("reduce(%p, 8, %p): %d\n",
+     (void *)arr, (void *)max, reduce(arr, 8, max));
 
     return 0;
 }
 
-int reduce(int arr[], int n) {
+int reduce(int arr[], int n, int (*fn)(int, int)) {
     int val = arr[0], i;
 
+    /* NOTE: Finding the max element in an array is very similar to finding the
+     *       sum of all elements in an array: both involve reducing the
+     *       elements into a single value by repeatedly applying some operation
+     *       to combine two elements. */
     for (i = 1; i < n; i++) {
-        val = val + arr[i];
+        val = (*fn)(val, arr[i]);
     }
 
     return val;
