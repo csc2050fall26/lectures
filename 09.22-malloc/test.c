3bb8c091665832d9e2515a9720f3d0b16380e433
Christopher E. Siu <cesiu@calpoly.edu>
2026-09-22 15:35:28 -0700

    Added example of malloc.

--- 09.22-malloc/test.c
+++ 09.22-malloc/test.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 
 int *pair(int, int);
+void f(void);
 
 int main(void) {
     int *arr;
 
     arr = pair(1, 2);
+    f();
+
     printf("arr: %p\n", (void *)arr);
     printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
     printf(" +- %p: %d\n", (void *)&arr[1], arr[1]);
@@ -15,10 +18,27 @@ int main(void) {
 }
 
 int *pair(int first, int second) {
-    int arr[2];
+    /* NOTE: Since the value of an array is the address of its first element,
+     *       returning an array returns the address of memory that has been
+     *       deallocated and may be overwritten in the future.
+     * int arr[2]; */
+
+    /* NOTE: "malloc" returns a pointer to a contiguous block of memory of the
+     *       requested size on the heap, and since pointer and array syntax is
+     *       largely interchangeable, that block can then be indexed. */
+    int *arr = (int *)malloc(sizeof(int) * 2);
 
     arr[0] = first;
     arr[1] = second;
 
     return arr;
+}
+
+void f(void) {
+    /* NOTE: Since "f" has the same locals as "pair", and since "f" is called
+     *       by the same function as "pair", in all likelihood this local array
+     *       will reuse the same memory as the one above. */
+    int arr[2] = {0};
+
+    (void)arr;
 }
